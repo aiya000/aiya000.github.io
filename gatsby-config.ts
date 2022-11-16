084e6155cd89fdd0a3efe535b2bf285769385d04
aiya000 <aiya000.develop@gmail.com>
2022-11-16 18:36:20 +0900

    :up: Install Gatsby Image Plugin

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,20 +2,20 @@ import type { GatsbyConfig } from 'gatsby'
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `galaxy-sixth-sensey`,
-    siteUrl: `https://www.yourdomain.tld`,
+    title: 'galaxy-sixth-sensey',
+    siteUrl: 'https://aiya000.github.io',
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
   plugins: [
-    'gatsby-plugin-postcss',
     'gatsby-plugin-image',
     'gatsby-plugin-mdx',
+    'gatsby-plugin-postcss',
     'gatsby-plugin-sharp',
+    'gatsby-plugin-vanilla-extract',
     'gatsby-transformer-sharp',
-    `gatsby-plugin-vanilla-extract`,
     {
       resolve: 'gatsby-source-filesystem',
       options: {
